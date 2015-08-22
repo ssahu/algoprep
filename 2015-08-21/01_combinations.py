caebd945bd9245e69fb081be1db76556ba62a97f
Sarthak Sahu <sarthaksahu1988@gmail.com>
2015-08-22 09:21:37 -0700

    Make it simpler
    - updated the time and space complexity

--- 2015-08-21/01_combinations.py
+++ 2015-08-21/01_combinations.py
@@ -1,26 +1,26 @@
-# space complexity is O(n!)
-# time complexity is O(n^2) 
+# space complexity is O(n)
+# time complexity is O(2^n)
 import sys
 
 def recursion_combination(s, length):
-#   print s, length
-  if len(s) == 0 or length == 0:
+  if length == 0:
   	return ['']
+  if len(s) < length:
+    return []
   combinations = []
   for char, idx in zip(s, range(len(s))):
-    combinations = combinations + [char + t for t in recursion_combination(s[idx+1:], length - 1) if len(t) == length - 1]
+    combinations = combinations + [char + t for t in recursion_combination(s[idx+1:], length - 1)]
   return combinations
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
     s = sys.argv[1]
     combinations = []
-    print s
+    print 'input string:', s
     for i in range(len(s))[1:]:
-    	print recursion_combination(s, i)
     	combinations = combinations + recursion_combination(s, i)
-    print 'combinations: ', combinations
-    print 'total number of combinations: ', len(combinations)
+    print 'combinations:', combinations
+    print 'total number of combinations:', len(combinations)
   else:
     print 'input a word'
     print 'run the program as:'
