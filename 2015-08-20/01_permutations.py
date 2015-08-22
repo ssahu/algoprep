6f3a06dee8fcf2eab3c9a26d6906261a8fd8eeae
Sarthak Sahu <sarthaksahu1988@gmail.com>
2015-08-22 09:36:48 -0700

    Remove usage of keyword as variable

--- 2015-08-20/01_permutations.py
+++ 2015-08-20/01_permutations.py
@@ -2,29 +2,29 @@
 # time complexity is n! - n being the length of string
 import sys
 
-def recursion_permutation(str):
+def recursion_permutation(s):
   permutations = []
-  if len(str) > 2:
+  if len(s) > 2:
 #    	if string length is more than 2
 # 		then pick each character and recurse
 # 		on the remaining characters
-    for k, idx in zip(str, range(len(str))):
+    for k, idx in zip(s, range(len(s))):
 #     	after picking one character recurse on remaining string
 #       example: if you have string 'abc'
 #       when you pick 'a', recurse on 'bc'
 #       when you pick 'b', recurse on 'ac'
-      permutations_each_char = recursion_permutation(str[:idx] + str[idx+1:])
+      permutations_each_char = recursion_permutation(s[:idx] + s[idx+1:])
       permutations = permutations + [k + t for t in permutations_each_char]
-  elif len(str) == 2:
+  elif len(s) == 2:
 #     if length of string is 2
 #     only 2 permutations possible
 #     exacmple: if you have string 'ab'
 #     permutations possible 'ab', 'ba'
-    permutations = [str, str[::-1]]
-  elif len(str) == 1:
+    permutations = [s, s[::-1]]
+  elif len(s) == 1:
 #     if string length is 1
 #     only that string is the only permutation
-    permutations = [str]
+    permutations = [s]
   return permutations
 
 if __name__ == '__main__':
